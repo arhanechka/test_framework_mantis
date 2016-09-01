f543301a4b21a1e4263c1c57ef34fda8121eb00b
Loki_ <annadvarri@gmail.com>
2016-09-01 17:57:16 +0300

    choose browser

--- src/main/java/mantis/for_tests/ChooseBrowser.java
+++ src/main/java/mantis/for_tests/ChooseBrowser.java
@@ -1,7 +1,15 @@
 package mantis.for_tests;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by Loki_ on 31-Aug-16.
  */
 public class ChooseBrowser {
+
+    public void chooseBrowsers {
+
+        driver = new ChromeDriver();
+
+    }
 }
