6b1b023e4ccb9fb3819602c522a052e376e75760
Loki_ <annadvarri@gmail.com>
2016-09-01 17:45:15 +0300

    page for login

--- src/main/java/mantis/pages/LoginPage.java
+++ src/main/java/mantis/pages/LoginPage.java
@@ -7,40 +7,54 @@ import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
- * Created by Олег on 24.08.2016.
+ * Created by Loki_ on 24.08.2016.
  */
 public class LoginPage {
     private WebDriver driver;
     private static Logger log = LoggerFactory.getLogger(LoginPage.class);
-    String URL = "http://manzhos.me/mantis/login_page.php";
+
     // Login
     @FindBy (name="username")
     private WebElement userName;
 
     // Passwort
     @FindBy (name="passwort")
-    private WebElement passwort;
+    private WebElement passWort;
 
     // Submit
     @FindBy(className="button")
     private WebElement loginButton;
 
-    public LoginPage(WebDriver driver) {
+    public LoginPage(WebDriver driver, WebElement userName, WebElement passwort, WebElement loginButton) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
+        this.userName = userName;
+        this.passWort = passwort;
+        this.loginButton = loginButton;
     }
 
-    private void getLogin(WebDriver driver, String login, String pass) {
+    public void clickLoginDev(WebDriver driver, String login, String pass) {
         userName.clear();
         userName.sendKeys(login);
-        log.debug("Login was entered: {}", userName);
-        passwort.clear();
-        passwort.sendKeys(pass);
-        log.debug("Pass was entered: {}", passwort);
+        log.debug("Login as Dev was entered: {}", userName);
+        passWort.clear();
+        passWort.sendKeys(pass);
+        log.debug("Pass as Dev was entered: {}", passWort);
         loginButton.click();
         log.info("The 'Login' button was submited");
     }
+
+    public void clickLoginManager(WebDriver driver, String login, String pass) {
+        userName.clear();
+        userName.sendKeys(login);
+        log.debug("Login as Manager was entered: {}", userName);
+        passWort.clear();
+        passWort.sendKeys(pass);
+        log.debug("Pass as Manager was entered: {}", passWort);
+        loginButton.click();
+        log.info("The 'Login' button was submited");
+    }
+
 
 }
