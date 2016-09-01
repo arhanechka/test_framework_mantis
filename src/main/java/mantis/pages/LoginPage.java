c7153f5b126d65aebb8c9939c85b7d3a0e8129a4
Loki_ <annadvarri@gmail.com>
2016-09-01 11:51:02 +0300

    init elements on login page

--- src/main/java/mantis/pages/LoginPage.java
+++ src/main/java/mantis/pages/LoginPage.java
@@ -1,22 +1,35 @@
 package mantis.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * Created by Олег on 24.08.2016.
  */
 public class LoginPage {
     private WebDriver driver;
+    private static Logger log = LoggerFactory.getLogger(LoginPage.class);
+    String URL = "http://manzhos.me/mantis/login_page.php";
     // Login
     @FindBy (name="username")
-    private WebElement inputElement;
+    private WebElement userName;
+    log.debug("Login was entered: {}", userName);
     // Passwort
     @FindBy (name="passwort")
-    private WebElement getInputElement;
+    private WebElement pass;
+    log.debug("Pass was entered: {}", pass);
     // Submit
-    @FindBy (className="button")
-    private WebElement getInputElement;
+    @FindBy(className="button")
+    private WebElement loginButton;
+    log.info("The 'Login' button was submited");
 
     public loginPage(WebDriver driver) {
-        Pagefactory.initElements(driver, this);
+        PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 }
