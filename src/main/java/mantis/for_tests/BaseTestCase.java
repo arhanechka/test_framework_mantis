7a9c0ae32f86ad7ce4b8da1a276f05e736db87af
Loki_ <annadvarri@gmail.com>
2016-09-01 17:44:05 +0300

    test class

---
+++ src/main/java/mantis/for_tests/BaseTestCase.java
@@ -0,0 +1,7 @@
+package mantis.for_tests;
+
+/**
+ * Created by User on 01-Sep-16.
+ */
+public class BaseTestCase {
+}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,7 +1,21 @@
 package tests;
 
+import mantis.for_tests.BaseTestCase;
+import mantis.for_tests.GetLogin;
+import mantis.pages.LoginPage;
+import org.junit.Test;
+
 /**
- * Created by Олег on 24.08.2016.
+ * Created by Loki_ on 24.08.2016.
  */
-public class LoginTest {
+public class LoginTest extends BaseTestCase {
+    public static String expectedResult = "";
+
+    @Test
+    public void loginTest() {
+        LoginPage logPass = new GetLogin();
+        logPass.getEnterDev(driver, " ", " ");
+
+
+    }
 }
