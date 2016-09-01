3791eb21a092f31af8ff5ba9f2502f26c9ae17ab
Loki_ <annadvarri@gmail.com>
2016-09-01 17:56:16 +0300

    base test case

--- src/main/java/mantis/for_tests/BaseTestCase.java
+++ src/main/java/mantis/for_tests/BaseTestCase.java
@@ -1,7 +1,30 @@
 package mantis.for_tests;
 
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+import mantis.pages.LoginPage;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 /**
- * Created by User on 01-Sep-16.
+ * Created by Loki_ on 01-Sep-16.
  */
 public class BaseTestCase {
+    public WebDriver driver;
+    private String URL = "http://manzhos.me/mantis/login_page.php";
+
+
+    @Before
+    public void getLogPage() throws InterruptedException {
+
+        PageFactory.initElements(driver, LoginPage.class);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(URL);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
