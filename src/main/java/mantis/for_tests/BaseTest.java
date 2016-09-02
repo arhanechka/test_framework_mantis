3fdaebe624723b962fbdce5773588df37922e970
Loki_ <annadvarri@gmail.com>
2016-09-02 15:07:07 +0300

    base test class

--- src/main/java/mantis/for_tests/BaseTest.java
+++ src/main/java/mantis/for_tests/BaseTest.java
@@ -4,7 +4,6 @@ import mantis.pages.LoginPage;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.concurrent.TimeUnit;
