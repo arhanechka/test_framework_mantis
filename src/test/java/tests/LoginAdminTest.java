3c021de2ae4acdb3324a9c45894b1615a8d1f780
Loki_ <annadvarri@gmail.com>
2016-09-02 16:57:09 +0300

    +++

--- src/test/java/tests/LoginAdminTest.java
+++ src/test/java/tests/LoginAdminTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import mantis.for_tests.BaseTest;
 import mantis.pages.LoginPage;
+import mantis.pages.SuccessfulLoginPage;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,15 +10,21 @@ import org.junit.Test;
  * Created by Loki_ on 02-Sep-16.
  */
 public class LoginAdminTest extends BaseTest {
-    public static String expectedResult = "";
+    public static String expectedResult = "Logout";
 
     @Test
     public void loginTest() {
         LoginPage logPass = new LoginPage(driver);
         logPass.clickLoginAdmin();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        SuccessfulLoginPage sucLogin = new SuccessfulLoginPage(driver);
+        sucLogin.clickLogOut();
+        String actualResult = logPass.clickLogOut();
 
-
-
-        Assert.assertEquals("Login as Admin", expectedResult, "");
+        Assert.assertEquals("Login as Admin", expectedResult, actualResult);
     }
 }
