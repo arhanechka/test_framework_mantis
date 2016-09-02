723ba693a5c17bf21330ad0d512139d4f5f857ab
Loki_ <annadvarri@gmail.com>
2016-09-02 16:28:33 +0300

    Mega algorithm to select a browser

--- src/main/java/mantis/for_tests/ChooseBrowser.java
+++ src/main/java/mantis/for_tests/ChooseBrowser.java
@@ -1,15 +1,43 @@
 package mantis.for_tests;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by Loki_ on 31-Aug-16.
  */
 public class ChooseBrowser {
+    private static final String sFileName = "testBrowser.properties";
+    private static String sDirSeparator = System.getProperty("file.separator");
+    private static Properties props = new Properties();
 
-    public void chooseBrowsers {
+    public void chooseBrowsers(WebDriver driver) throws IOException {
+        File currentDir = new File(".");
 
-        //driver = new ChromeDriver();
+        String sFilePath = currentDir.getCanonicalPath() + sDirSeparator + sFileName;
+        FileInputStream ins = new FileInputStream(sFilePath);
+        props.load(ins);
+        System.out.println(props.getProperty("BROWSER"));
+        String browser = props.getProperty("BROWSER");
 
+        if (browser.equals("Chrome")) {
+            driver = new ChromeDriver();
+            return;
+        }
+        if (browser.equals("FF")) {
+            driver = new FirefoxDriver();
+            return;
+        }
+        if (browser.equals("IE")) {
+            driver = new InternetExplorerDriver();
+            return;
+        }
     }
 }
