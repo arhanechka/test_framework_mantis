52d9f1751a0bad7cacc4f4363a7e3599490b6ead
Loki_ <annadvarri@gmail.com>
2016-09-02 15:35:09 +0300

    +++

--- src/test/java/tests/LoginDevTest.java
+++ src/test/java/tests/LoginDevTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import mantis.for_tests.BaseTest;
 import mantis.pages.LoginPage;
+import mantis.pages.SuccessfulLoginPage;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,15 +10,22 @@ import org.junit.Test;
  * Created by Loki_ on 24.08.2016.
  */
 public class LoginDevTest extends BaseTest {
-    public static String expectedResult = "";
+    private static String expectedResult = "Logout";
 
     @Test
-    public void loginTest() {
+    public void loginTest() throws InterruptedException {
         LoginPage logPass = new LoginPage(driver);
         logPass.clickLoginDev();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        SuccessfulLoginPage sucLogin = new SuccessfulLoginPage(driver);
+        sucLogin.clickLogOut();
+        String actualResult = logPass.chooseResult();
 
 
-
-        Assert.assertEquals("Login as Dev", expectedResult, "");
+        Assert.assertEquals("Login as Dev", expectedResult, actualResult);
     }
 }
