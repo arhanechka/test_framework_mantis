7b46909d72fa0d11e4d61cf0dba82bb23952e7b3
arhanechka <arhanechka@gmail.com>
2016-09-04 15:17:42 +0200

    test was connected to method clickLoginAdmin

--- src/test/java/tests/CreateNewUserTest.java
+++ src/test/java/tests/CreateNewUserTest.java
@@ -21,16 +21,14 @@ public class CreateNewUserTest {
     private static final String URL1 = "http://www.manzhos.me/mantis/manage_user_create_page.php";
     private static WebDriver driver;
     private static LoginPage a;
-    private  static String adminLogin="admin_auto";
-    private static String adminPassword="Administr1";
     private  static CreateUserChecking trying;
-    private  static RandomNames randomnames;
+    private  static RandomNames random=new RandomNames();;
     By mistakeLocator=By.className("form-title");
     private  final String email ="tests_for_selenium@mail.ru";
-    private  String randomLogin=randomnames.getNewRandomName()+"_auto";
+    private  String randomLogin=random.getNewRandomName()+"_auto";
     private  static MailRuReader activator;
     private  static PasswordConfirm passConfirm;
-  //  private static Logger log = LoggerFactory.getLogger(CreateNewUserTest.class);
+//    private static Logger log = LoggerFactory.getLogger(CreateNewUserTest.class);
 
     @BeforeClass
     public static void setUp() {
@@ -38,12 +36,11 @@ public class CreateNewUserTest {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(URL);
         a=new LoginPage(driver);
-        a.clickLoginAdmin(adminLogin,adminPassword);
-     //   log.info("Enter as admin was successfull");
+        a.clickLoginAdmin();
+       // log.info("Enter as admin was successfull");
         trying=new CreateUserChecking(driver);
         activator=new MailRuReader(driver);
-        randomnames=new RandomNames();
-//passConfirm=activator.MailActivator();
+
     }
     @Before
     public void openUrl() {
@@ -60,39 +57,39 @@ public class CreateNewUserTest {
     }
     @Test
     public void loginWithEmptyEmail(){
-        String rand=randomnames.getNewRandomName()+"_auto";
+        String rand=random.getNewRandomName()+"_auto";
         Assert.assertEquals(trying.creationChecking(rand,"", mistakeLocator).getText(),"APPLICATION ERROR #1200");
-   //     log.info("Empty email and login "+ rand+" were typed");
-         }
+       // log.info("Empty email and login "+ rand+" were typed");
+    }
     @Test
     public void loginWithEmptyName(){
 
         Assert.assertEquals(trying.creationChecking("",email, mistakeLocator).getText(),"APPLICATION ERROR #11");
       //  log.info("Empty  login and email "+ email+" were typed");
-             }
+    }
     @Test
     public void loginWithWrongEmail(){
-        String randomLog=randomnames.getNewRandomName()+"_auto";
-        String randomPassword=randomnames.getNewRandomName();
+        String randomLog=random.getNewRandomName()+"_auto";
+        String randomPassword=random.getNewRandomName();
         Assert.assertEquals(trying.creationChecking(randomLog,randomPassword, mistakeLocator).getText(),"APPLICATION ERROR #1200");
-    //    log.info("login "+randomLog+" and email "+randomPassword+" were typed");
-        }
+       // log.info("login "+randomLog+" and email "+randomPassword+" were typed");
+    }
     @Test
     public void withRightDates() throws InterruptedException {
         Assert.assertEquals(trying.creationChecking(randomLogin,email, By.xpath("//div[@align='center']")).getText(),"Created user "+randomLogin+" with an access level of reporter\n[ Proceed ]");
-     //   log.info("login "+randomLogin+" and email "+email+" were typed");
+      //  log.info("login "+randomLogin+" and email "+email+" were typed");
         passConfirm=activator.letterActivation();
-        String randomPassword=randomnames.getNewRandomName();
+        String randomPassword=random.getNewRandomName();
         Assert.assertEquals(passConfirm.passwordEntering(randomPassword),"Password successfully updated\n" +"Operation successful.\n" + "[ Proceed ]");
-   //     log.info("Password was successfully typed and user was created correctly");
+      //  log.info("Password was successfully typed and user was created correctly");
         activator.letterDeleting();
         driver.get(URL);
-        a.clickLoginAdmin(adminLogin,adminPassword);
+        a.clickLoginAdmin();
         driver.get(URL1);
-    }
+   }
 
 
-@AfterClass
+    @AfterClass
     public static void tearDown() {
         if (driver != null) {
             driver.close();
