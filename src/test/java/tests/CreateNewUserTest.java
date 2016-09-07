9d318e6ab7b74b4c07fd9ea2c4ca3d8963ea97f7
arhanechka <arhanechka@gmail.com>
2016-09-07 22:41:07 +0200

    log was activated

--- src/test/java/tests/CreateNewUserTest.java
+++ src/test/java/tests/CreateNewUserTest.java
@@ -24,11 +24,11 @@ public class CreateNewUserTest {
     private  static CreateUserChecking trying;
     private  static RandomNames random=new RandomNames();;
     By mistakeLocator=By.className("form-title");
-    private  final String email ="tests_for_selenium@mail.ru";
+    private  final String email ="new_tests_for_selenium@mail.ru";
     private  String randomLogin=random.getNewRandomName()+"_auto";
     private  static MailRuReader activator;
     private  static PasswordConfirm passConfirm;
-//    private static Logger log = LoggerFactory.getLogger(CreateNewUserTest.class);
+    private static Logger log = LoggerFactory.getLogger(CreateNewUserTest.class);
 
     @BeforeClass
     public static void setUp() {
@@ -37,7 +37,7 @@ public class CreateNewUserTest {
         driver.get(URL);
         a=new LoginPage(driver);
         a.clickLoginAdmin();
-       // log.info("Enter as admin was successfull");
+        log.info("Enter as admin was successfull");
         trying=new CreateUserChecking(driver);
         activator=new MailRuReader(driver);
 
@@ -53,35 +53,36 @@ public class CreateNewUserTest {
     public void loginAsEmptyUser(){
 
         Assert.assertEquals(trying.creationChecking("","",mistakeLocator).getText(),"APPLICATION ERROR #11");
-     //   log.info("Empty login and email were typed");
+        log.info("Empty login and email were typed");
     }
     @Test
     public void loginWithEmptyEmail(){
         String rand=random.getNewRandomName()+"_auto";
         Assert.assertEquals(trying.creationChecking(rand,"", mistakeLocator).getText(),"APPLICATION ERROR #1200");
-       // log.info("Empty email and login "+ rand+" were typed");
+        log.info("Empty email and login "+ rand+" were typed");
     }
     @Test
     public void loginWithEmptyName(){
 
         Assert.assertEquals(trying.creationChecking("",email, mistakeLocator).getText(),"APPLICATION ERROR #11");
-      //  log.info("Empty  login and email "+ email+" were typed");
+       log.info("Empty  login and email "+ email+" were typed");
     }
     @Test
     public void loginWithWrongEmail(){
         String randomLog=random.getNewRandomName()+"_auto";
         String randomPassword=random.getNewRandomName();
         Assert.assertEquals(trying.creationChecking(randomLog,randomPassword, mistakeLocator).getText(),"APPLICATION ERROR #1200");
-       // log.info("login "+randomLog+" and email "+randomPassword+" were typed");
+        log.info("login "+randomLog+" and email "+randomPassword+" were typed");
     }
     @Test
-    public void withRightDates() throws InterruptedException {
+    public void withRightDates() {
         Assert.assertEquals(trying.creationChecking(randomLogin,email, By.xpath("//div[@align='center']")).getText(),"Created user "+randomLogin+" with an access level of reporter\n[ Proceed ]");
-      //  log.info("login "+randomLogin+" and email "+email+" were typed");
+        log.info("login "+randomLogin+" and email "+email+" were typed");
+
         passConfirm=activator.letterActivation();
         String randomPassword=random.getNewRandomName();
         Assert.assertEquals(passConfirm.passwordEntering(randomPassword),"Password successfully updated\n" +"Operation successful.\n" + "[ Proceed ]");
-      //  log.info("Password was successfully typed and user was created correctly");
+        log.info("Password "+randomPassword+ " was successfully typed and user was created correctly");
         activator.letterDeleting();
         driver.get(URL);
         a.clickLoginAdmin();
@@ -89,9 +90,9 @@ public class CreateNewUserTest {
    }
 
 
-    @AfterClass
+  /*  @AfterClass
     public static void tearDown() {
         if (driver != null) {
             driver.close();
             driver.quit();
-        }}}
+        }}*/}
