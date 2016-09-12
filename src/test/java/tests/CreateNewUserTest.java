2bdd6029b7fb9ede7651b94967ab80ee18d1060a
arhanechka <arhanechka@gmail.com>
2016-09-12 23:34:51 +0200

    comments were added

--- src/test/java/tests/CreateNewUserTest.java
+++ src/test/java/tests/CreateNewUserTest.java
@@ -25,14 +25,14 @@ public class CreateNewUserTest {
     private static final String URL = "http://www.manzhos.me/mantis";
     private static final String URL1 = "http://www.manzhos.me/mantis/manage_user_create_page.php";
     private static WebDriver driver;
-    private static LoginPage a;
-    private  static CreateUserChecking trying;
-    private  static RandomNames random=new RandomNames();;
+    private static LoginPage a; //first pageObject for enter
+    private  static CreateUserChecking trying; //pageObject with fields for new user creation
+    private  static RandomNames random=new RandomNames(); //class for creation random names and passwords
     By mistakeLocator=By.className("form-title");
-    private  final String email ="new_tests_for_selenium@mail.ru";
+    private  final String email ="selmanium@mail.ru";
     private  String randomLogin=random.getNewRandomName()+"_auto";
-    private  static MailRuReader activator;
-    private  static PasswordConfirm passConfirm;
+    private  static MailRuReader activator; //class for work with mail.ru
+    private  static PasswordConfirm passConfirm; //pageObject where password confirms
     private static Logger log = LoggerFactory.getLogger(CreateNewUserTest.class);
 
     @BeforeClass
@@ -41,7 +41,7 @@ public class CreateNewUserTest {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(URL);
         a=new LoginPage(driver);
-        a.clickLoginAdmin();
+        a.clickLoginAdmin();//enter as admin
         log.info("Enter as admin was successfull");
         trying=new CreateUserChecking(driver);
         activator=new MailRuReader(driver);
@@ -53,25 +53,28 @@ public class CreateNewUserTest {
         driver.get(URL1);
 
     }
-
+// Checking enter with empty dates
     @Test
     public void loginAsEmptyUser(){
 
         Assert.assertEquals(trying.creationChecking("","",mistakeLocator).getText(),"APPLICATION ERROR #11");
         log.info("Empty login and email were typed");
     }
+    //Checking enter only with login
     @Test
     public void loginWithEmptyEmail(){
         String rand=random.getNewRandomName()+"_auto";
         Assert.assertEquals(trying.creationChecking(rand,"", mistakeLocator).getText(),"APPLICATION ERROR #1200");
         log.info("Empty email and login "+ rand+" were typed");
     }
-    @Test
+   // Checking enter only with email
+     @Test
     public void loginWithEmptyName(){
 
         Assert.assertEquals(trying.creationChecking("",email, mistakeLocator).getText(),"APPLICATION ERROR #11");
        log.info("Empty  login and email "+ email+" were typed");
     }
+    //Checking enter with random login and wrong constructed email
     @Test
     public void loginWithWrongEmail(){
         String randomLog=random.getNewRandomName()+"_auto";
@@ -79,6 +82,7 @@ public class CreateNewUserTest {
         Assert.assertEquals(trying.creationChecking(randomLog,randomPassword, mistakeLocator).getText(),"APPLICATION ERROR #1200");
         log.info("login "+randomLog+" and email "+randomPassword+" were typed");
     }
+    //Checking enter with random login and email mail.ru
     @Test
     public void withRightDates() {
         Assert.assertEquals(trying.creationChecking(randomLogin,email, By.xpath("//div[@align='center']")).getText(),"Created user "+randomLogin+" with an access level of reporter\n[ Proceed ]");
