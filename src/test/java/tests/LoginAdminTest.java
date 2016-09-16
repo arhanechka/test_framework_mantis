5f7ed0eadc6aedbfd6a4c65bf750bb7e38882455
Loki_ <annadvarri@gmail.com>
2016-09-16 13:32:34 +0300

    small changes in tests 16th sept

--- src/test/java/tests/LoginAdminTest.java
+++ src/test/java/tests/LoginAdminTest.java
@@ -1,29 +1,27 @@
 package tests;
 
-import mantis.for_tests.BaseTest;
+import mantis.for_tests.BaseTestCase;
 import mantis.pages.LoginPage;
 import mantis.pages.SuccessfulLoginPage;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * Created by Loki_ on 02-Sep-16.
  */
-public class LoginAdminTest extends BaseTest {
-    private WebDriver driver;
+public class LoginAdminTest extends BaseTestCase {
     public static String expectedResult = "Logout";
 
     @Test
     public void loginTest() {
-        LoginPage logPass = new LoginPage(driver);
+        LoginPage logPass = new LoginPage(getDriver());
         logPass.clickLoginAdmin();
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        SuccessfulLoginPage sucLogin = new SuccessfulLoginPage(driver);
+        SuccessfulLoginPage sucLogin = new SuccessfulLoginPage(getDriver());
         sucLogin.clickLogOut();
         LoginPage actualResult = logPass.textLoginButton();
 
