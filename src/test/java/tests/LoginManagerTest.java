8543fa4f7d8650df93b4be95ce382cf80a38e6f2
arhanechka <arhanechka@gmail.com>
2016-09-17 18:39:11 +0200

    small changes

--- src/test/java/tests/LoginManagerTest.java
+++ src/test/java/tests/LoginManagerTest.java
@@ -1,7 +1,9 @@
 package tests;
 
 import mantis.for_tests.BaseTest;
+import mantis.for_tests.BaseTestCase;
 import mantis.pages.LoginPage;
+import mantis.pages.MyViewPage;
 import mantis.pages.SuccessfulLoginPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -10,23 +12,20 @@ import org.testng.annotations.Test;
 /**
  * Created by Loki_ on 02-Sep-16.
  */
-public class LoginManagerTest extends BaseTest {
-    private WebDriver driver;
+public class LoginManagerTest extends BaseTestCase {
     public static String expectedResult = "Logout";
 
     @Test
     public void loginTest() {
-        LoginPage logPass = new LoginPage(driver);
+        LoginPage logPass = new LoginPage(getDriver());
         logPass.clickLoginManager();
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        SuccessfulLoginPage sucLogin = new SuccessfulLoginPage(driver);
-        sucLogin.clickLogOut();
-        LoginPage actualResult = logPass.textLoginButton();
-
-        Assert.assertEquals("Login as Manager", actualResult, expectedResult);
+        MyViewPage logout=new MyViewPage(getDriver());
+        String actualResult = logout.getLogoutText();
+        Assert.assertEquals(expectedResult, actualResult, "Login as Manager");
     }
 }
