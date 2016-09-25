c9614dada2e4586fc42156756b8eefe882626eb5
Svetlana <stepanovasveta.ua@gmail.com>
2016-09-26 00:58:08 +0300

    create WrongValidateBugReportPage

---
+++ src/main/java/mantis/pages/WrongValidateBugReportPage.java
@@ -0,0 +1,26 @@
+package mantis.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by sveta on 24.09.16.
+ */
+public class WrongValidateBugReportPage extends AbstractPage {
+    @FindBy (className="form-title")
+    WebElement mistakeValidate;
+
+    public WrongValidateBugReportPage (WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);}
+
+    public String getMistakeText (){
+
+        return mistakeValidate.getText();
+    }
+}
+
+
+
