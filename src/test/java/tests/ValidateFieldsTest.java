84b67782fd3c09a7972e2eed68e9978a81d45eee
Svetlana <stepanovasveta.ua@gmail.com>
2016-09-29 19:30:30 +0300

    Message

--- src/test/java/tests/ValidateFieldsTest.java
+++ src/test/java/tests/ValidateFieldsTest.java
@@ -1,47 +1,48 @@
 package tests;
 
 import mantis.for_tests.BaseTestCase;
-import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by sveta on 19.09.16.
  */
 public class ValidateFieldsTest extends BaseTestCase {
-
     private static Logger log = LoggerFactory.getLogger (ValidateFieldsTest.class);
 
-
-    static WebDriver driver;
-
-
     @Test
+    public void TestPassRequeriedFields() {
+        validateFieldsPage.fillOutRequiredFields (
+                "test summ",
+                "test descr", "test steps",
+                "test addInfo");
+        driver.manage ().timeouts ().implicitlyWait (1, TimeUnit.SECONDS);
+        Assert.assertEquals (driver.getTitle (), "View Issues - MantisBT");
+    }
 
-    public void checkFillOutRequariedFields() {
+
+    //checking - not all of the required fields are mandatory
+    @Test
+    public void TestReportFailed() {
         validateFieldsPage.fillOutRequiredFields (
                 "test summ",
                 "test descr",
-                "test steps",
+                "",
                 "test addInfo");
+        driver.manage ().timeouts ().implicitlyWait (1, TimeUnit.SECONDS);
         Assert.assertEquals (driver.getTitle (), "MantisBT");
-
-        // Assert.assertEquals (driver.getTitle (), "View Issues - MantisBT");
-
-        //"APPLICATION ERROR #11"); // тест проходит , так как не выполняется Select category
-
-
     }
+
+
 
     @Test
-
-    public void checkInputEmptyData() {
-        Assert.assertEquals (validateFieldsPage.fillOutRequiredFields ("", "", "", ""), "APPLICATION ERROR #11");
+    public void TestEmptyFields() {
+        Assert.assertEquals (validateFieldsPage.
+                        fillOutRequiredFields ("", "", "", ""),
+                "APPLICATION ERROR #11");
     }
-
-
 }
-
-
